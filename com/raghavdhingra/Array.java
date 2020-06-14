14014c5e216151e6c34039515cc9979067a66917
Raghav <raghav.dhingra15@gmail.com>
2020-06-15 00:12:13 +0530

    initial

---
+++ com/raghavdhingra/Array.java
@@ -0,0 +1,21 @@
+package com.raghavdhingra;
+
+import java.util.Arrays;
+
+public class Array {
+    private int[] numberArray;
+    private int count;
+
+    public Array(int length){
+        numberArray = new int[length];
+    }
+
+    public void print(){
+        System.out.println(Arrays.toString(numberArray));
+    }
+    public void insert(){
+        for (int i = 0;i<count;i++){
+
+        }
+    }
+}
---
+++ com/raghavdhingra/Main.java
@@ -0,0 +1,9 @@
+package com.raghavdhingra;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Array number = new Array(3);
+        number.print();
+    }
+}
