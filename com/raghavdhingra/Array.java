64d7304515e62092a955f82959643d8fd6248da4
Raghav <raghav.dhingra15@gmail.com>
2020-06-15 01:12:00 +0530

    array operations

--- com/raghavdhingra/Array.java
+++ com/raghavdhingra/Array.java
@@ -6,30 +6,36 @@ public class Array {
     private int[] numberArray;
     private int count;
 
-    public Array(int length){
+    public Array(int length) {
         numberArray = new int[length];
     }
 
-    public void print(){
+    public void print() {
         System.out.println(Arrays.toString(numberArray));
     }
 
-    public void removeAt(int index){
-
-        for (int i=index;i<count;i++){
-            numberArray[i] = numberArray[i+1];
+    public void removeAt(int index) {
+        if (index < 0 || index >= count) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = index; i < count; i++) {
+            if (i+1 != count)
+                numberArray[i] = numberArray[i+1];
+            else
+                numberArray[i] = 0;
         }
         count--;
+        print();
     }
-
-    public void insert(int num){
-        if (numberArray.length == count){
+    public void insert(int num) {
+        if (numberArray.length == count) {
             int[] newArr = new int[count*2];
-            for (int i=0;i<count;i++){
+            for (int i=0;i<count;i++) {
                 newArr[i] = numberArray[i];
             }
             numberArray = newArr;
         }
         numberArray[count++] = num;
+        print();
     }
 }
--- com/raghavdhingra/Main.java
+++ com/raghavdhingra/Main.java
@@ -7,12 +7,10 @@ public class Main {
         number.insert(5);
         number.insert(6);
         number.insert(8);
-        number.print();
+        number.insert(9);
         number.insert(10);
-        number.removeAt(1);
-        number.print();
-        number.removeAt(1);
-        number.insert(5);
-        number.print();
+        number.insert(11);
+        number.removeAt(5);
+        number.insert(8);
     }
 }
