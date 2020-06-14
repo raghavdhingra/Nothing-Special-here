965cd4845515e25dbf3960afbadddbe44bf0922b
Raghav <raghav.dhingra15@gmail.com>
2020-06-15 00:48:01 +0530

    array operations

--- com/raghavdhingra/Array.java
+++ com/raghavdhingra/Array.java
@@ -13,9 +13,23 @@ public class Array {
     public void print(){
         System.out.println(Arrays.toString(numberArray));
     }
-    public void insert(){
-        for (int i = 0;i<count;i++){
 
+    public void removeAt(int index){
+
+        for (int i=index;i<count;i++){
+            numberArray[i] = numberArray[i+1];
         }
+        count--;
+    }
+
+    public void insert(int num){
+        if (numberArray.length == count){
+            int[] newArr = new int[count*2];
+            for (int i=0;i<count;i++){
+                newArr[i] = numberArray[i];
+            }
+            numberArray = newArr;
+        }
+        numberArray[count++] = num;
     }
 }
--- com/raghavdhingra/Main.java
+++ com/raghavdhingra/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Array number = new Array(3);
+        number.insert(5);
+        number.insert(6);
+        number.insert(8);
+        number.print();
+        number.insert(10);
+        number.removeAt(1);
+        number.print();
+        number.removeAt(1);
+        number.insert(5);
         number.print();
     }
 }
